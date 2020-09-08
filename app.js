59d4ccbb022e0166115707c1270352611c3f19b2
Shihwan Baek <45923522+sbdaniel37@users.noreply.github.com>
2020-09-08 14:29:43 -0700

    prepare codebase for heroku

--- app.js
+++ app.js
@@ -163,6 +163,11 @@ app.post('/delete', async (req, res) => {
 	}
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == '') {
+	port = 3000;
+}
+
+app.listen(port, function() {
 	console.log('Server is now running on port 3000.');
 });
